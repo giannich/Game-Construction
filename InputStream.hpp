60b766acbaebd885c6b4066efedd867c018e3c78
ragbinder <kozmary@gmail.com>
2017-04-15 18:46:58 -0500

    Require deltaTime for updateInputState. We're going to write our own InputQueue class too.

--- InputStream.hpp
+++ InputStream.hpp
@@ -16,15 +16,20 @@ public:
 	AccelerationAxisState acc;
 	FiringState fire;
 
-	InputState fromString(string str);
 	std::string toString();
+	InputState fromString(string str);
 };
 
 class InputStream {
 	int frameNumber;
-	std::queue<InputState> input;
+	InputQueue input;
+	
+	std::string toString();
+	InputStream fromString(std::string str);
 public:
-	virtual InputState updateInputState(GameState &gs) = 0;
+	// Need deltaTime to make sure that all machines are inputting at the same rate.
+	// If one machine is running slower, then it will fill its input by duplicating the most recent command to keep a constant framerate.
+	virtual InputState updateInputState(float deltaTime, GameState &gs) = 0;
 };
 
 class LocalPlayerInputStream: public InputStream {
@@ -32,19 +37,19 @@ public:
 	std::string hostIP;
 	// Need some SDL Data?
 	// Send commands to remote host if necessary
-	InputState updateInputState(GameState &gs);
+	InputState updateInputState(float deltaTime, GameState &gs);
 };
 
 class AIInputStream: public InputStream {
 public:
 	// This will have access to the input state from the last frame, and the gamestate for this frame.
 	// After this function is called, the currentState for the AIInputStream should be updated to what we want for next frame.
-	InputState updateInputState(GameState &gs);
+	InputState updateInputState(float deltaTime, GameState &gs);
 };
 
 class NetworkPlayerInputStream: public InputStream {
 	NetworkPlayer *socketInfo;
 public:
-	InputState updateInputState(GameState &gs);
+	InputState updateInputState(float deltaTime, GameState &gs);
 };
 
