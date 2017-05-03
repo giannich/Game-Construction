eb598601a9149111f1b15b362e42d00fb9fdd0af
ragbinder <kozmary@gmail.com>
2017-05-03 12:06:51 -0500

    Fixed empty file

---
+++ include/Soul.hpp
@@ -0,0 +1,11 @@
+
+#include "Box2d/Box.h"
+
+class Soul {
+	b2body *rigidBody
+	float collectionRadius;
+public:
+        rigidBody->SetAngularDamping(5.0f);
+	Soul(b2Vec2, float);
+}
+
---
+++ src/Soul.cpp
@@ -0,0 +1,18 @@
+
+#include "Soul.hpp"
+
+Soul::Soul(b2Vec2 initPos, float radius) {
+    //Create rigidbody
+    {
+        b2BodyDef bd;
+        bd.type = b2_dynamicBody;
+        bd.position.Set(initPos.x, initPos.y);
+        rigidBody = m_world.CreateBody(&bd);
+        
+        b2CircleShape shape(5.0);
+        
+        b2FixtureDef fd;
+        fd.shape = &shape;
+        rigidBody->CreateFixture(&fd);
+    }
+}
